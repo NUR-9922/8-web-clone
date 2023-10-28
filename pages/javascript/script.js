533170f0b904afd5934c88d908b6799f1893aace
NUSH-9922 <nush9922@gmail.com>
2023-10-28 20:10:54 +0530

    update

--- pages/javascript/script.js
+++ pages/javascript/script.js
@@ -1,27 +1,29 @@
 // // // locomotive js
-// const scroller = new LocomotiveScroll({
-//     el: document.querySelector('.mein'),
-//     smooth: true
-//   });
+const scroller = new LocomotiveScroll({
+    el: document.querySelector('.mein'),
+    smooth: true
+  });
+// 
 
-// progress bar 
-// gsap.registerPlugin(ScrollTrigger);
-// gsap.to('progress', {
-//   value: 100,
-//   ease: 'none',
-//   scrollTrigger: { scrub: 0.3 }
-// });
+// progress bar
+const progressAnimation = gsap.to('progress', {
+  value: 100,
+  ease: 'none',
+  scrollTrigger: {
+    scroller: new LocomotiveScroll({ el: document.querySelector('.mein'), smooth: true }),
+    scrub: 0.3,
+  },
+});
+
+progressAnimation.scrollTrigger.update();
 
 // // auto Number count 
-gsap.to(".page-6", {
-  opacity: 1,
-  dealy: 2,
+gsap.from(".page-6", {
   scrollTrigger: {
     trigger: ".page-6",
-    start: "top 100%", // When the top of the element reaches the center of the viewport
-    // When the bottom of the element reaches the center of the viewport
-    onEnter: autoNumber, // Function to run when the element enters the trigger area
-    onEnterBack: autoNumber, // Function to run when the element enters the trigger area from the bottom
+    start: "top 100%",
+    onEnter: autoNumber, 
+    onEnterBack: autoNumber, 
   },
 });
 function autoNumber() {
@@ -68,135 +70,126 @@ function autoNumber() {
   }
 
 }
-gsap.to(".page-1", {
-  opacity: 1,
-  dealy: 2,
+
+
+gsap.from(".page-1", {
   scrollTrigger: {
     trigger: ".page-1",
-    start: "top 100%", // When the top of the element reaches the center of the viewport
-    // When the bottom of the element reaches the center of the viewport
-    onEnter: pageOneAni, // Function to run when the element enters the trigger area
-    onEnterBack: pageOneAni, // Function to run when the element enters the trigger area from the bottom
+    start: "top 100%",
+    onEnter: pageOneAni, 
   },
 });
 function pageOneAni() {
 
   gsap.from(".page-1-right-left-part ", {
     x: 300,
-    opacity: 1,
-    duration: 2,
+    opacity: 0,
+    duration: 1,
   })
 
   gsap.from(".page-1-right-right-part-row-3 ", {
     y: -300,
-    opacity: 1,
-    duration: 2,
+    opacity: 0,
+    duration: 1,
   })
   gsap.from(".page-1-right-right-part-row-4", {
     y: 300,
-    opacity: 1,
+    opacity: 0,
     duration: 2,
   })
   gsap.from(".page-1-right-right-part-row-5", {
     scale: 0,
-    opacity: 1,
+    opacity: 0,
     duration: 2,
   })
 }
-gsap.to(".page-2", {
-  opacity: 1,
-  dealy: 2,
+gsap.from(".page-2", {
   scrollTrigger: {
     trigger: ".page-2",
     start: "top 100%",
     onEnter: pageTowAni,
-    onEnterBack: pageTowAni,
   },
 });
 function pageTowAni() {
   gsap.from(".page-2-left-box ", {
     x: 700,
-    opacity: 1,
-    duration: 2,
+    opacity: 0,
+    duration: .5,
   })
   gsap.from(".page-2-right", {
     x: -700,
-    opacity: 1,
-    duration: 2,
+    opacity: 0,
+    duration: .5,
   })
   gsap.from(".page-2-left-img", {
     x: -700,
-    opacity: 1,
-    duration: 2,
-    delay: 1
+    opacity: 0,
+    duration: .5,
+    delay: .5
   })
 
 }
-gsap.to(".page-3-row-2", {
-  opacity: 1,
-  dealy: 2,
+
+// pageThreeAni
+
+gsap.from(".page-3-row-2", {
   scrollTrigger: {
     trigger: ".page-3-row-2",
     start: "top 100%",
     onEnter: pageThreeAni,
-    onEnterBack: pageThreeAni,
   },
 });
 function pageThreeAni(){
   gsap.from(".page-3-row-2,.page-3-row-2-box", {
   y: 400,
-  opacity: 1,
-  duration: 2,
+  opacity: 0,
+  duration:1,
   stagger: 0.1
 })
 }
-gsap.to(".page-5", {
-  opacity: 1,
-  dealy: 2,
+// pageFiveAni
+
+gsap.from(".page-5", {
   scrollTrigger: {
     trigger: ".page-5",
     start: "top 100%",
     onEnter: pageFiveAni,
-    onEnterBack: pageFiveAni,
   },
 });
 
 function pageFiveAni(){
   gsap.from(".page-5-row-2-box", {
   y: 400,
-  opacity: 1,
-  duration: 2,
+  opacity: 0,
+  duration: 1,
   stagger: 0.1
 })
 };
 
-gsap.to(".page-6", {
-  opacity: 1,
-  dealy: 2,
+// pageSixAni
+gsap.from(".page-6", {
   scrollTrigger: {
     trigger: ".page-6",
     start: "top 100%",
     onEnter: pageSixAni,
-    onEnterBack: pageSixAni,
   },
 });
 function pageSixAni(){
   gsap.from(".page-6-left", {
   x: -400,
-  opacity: 1,
-  duration: 2,
+  duration: 1.5,
   stagger: 0.1
 })
 gsap.from(".page-6-right", {
   y: 400,
-  opacity: 1,
-  duration: 2,
+  opacity: 0,
+  duration: 1.5,
   stagger: 0.1
 })
 }
-gsap.to(".page-7", {
-  opacity: 1,
-  dealy: 2,
+
+// pageSevenAni
+gsap.from(".page-7", {
   scrollTrigger: {
     trigger: ".page-7",
     start: "top 100%",
@@ -207,7 +200,7 @@ gsap.to(".page-7", {
 function pageSevenAni(){
   gsap.from(".page-7-main", {
   y: 400,
-  opacity: 1,
-  duration: 2,
+  opacity: 0,
+  duration: 1,
   stagger: 0.1
 })}
